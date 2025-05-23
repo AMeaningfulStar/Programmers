bb03883dcdcea55b9b77ea2e05c054c441b41703
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2025-05-23 09:16:37 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 1.94 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        
+        for(String str:my_string.split("")) {
+            if(!str.equals(letter)) {
+                answer += str;
+            }
+        }
+        
+        return answer;
+    }
+}
