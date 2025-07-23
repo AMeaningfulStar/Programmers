4db8440a075d926bf4de5fbc0bf5e3bfed64c41f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2025-07-23 10:46:18 +0900

    [level 1] Title: 기사단원의 무기, Time: 128.61 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+
+		for (int i = 1; i <= number; i++) {
+			int count = 0;
+
+			for (int j = 1; j * j <= i; j++) {
+				if (i % j == 0) {
+					count += (j * j == i) ? 1 : 2; // 제곱수는 하나만 추가, 아니면 두 개
+				}
+			}
+
+			answer += count > limit ? power : count;
+		}
+
+		return answer;
+    }
+}
