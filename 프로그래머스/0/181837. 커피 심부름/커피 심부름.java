7e919683013a46fbff051ee5b35e6aeac8dfc72d
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-10 09:31:54 +0900

    [level 0] Title: 커피 심부름, Time: 0.71 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,17 +1,26 @@
+/*
+알고리즘: 선형 순회 + 조건 분기
+핵심 개념
+• 문자열 패턴 검사 (contains)
+• 조건에 따라 다른 값 누적
+시간 복잡도: O(N × M)
+• N = 주문 개수
+• M = 문자열 길이
+공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-		
-		for(String ord: order) {
-			if(ord.contains("americano")) {
-				answer += 4500;
-			} else if(ord.contains("cafelatte")) {
-				answer += 5000;
-			} else {
-				answer += 4500;
-			}
-		}
-		
-		return answer;
+
+        for(String menu : order) {
+            if(menu.contains("cafelatte")) {
+                answer += 5000;
+            } else {
+                answer += 4500;
+            }
+        }
+
+        return answer;
     }
 }
