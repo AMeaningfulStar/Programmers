fac6123eca3fde22f50d32966ce2c7ec870da5cb
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-11 17:23:44 +0900

    [level 0] Title: l로 만들기, Time: 8.70 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,15 +1,42 @@
+/*
+알고리즘: 문자열 선형 변환 (Linear Transformation)
+핵심 개념:
+• 문자열 순회
+• ASCII 문자 비교
+• 조건에 따라 문자 치환
+시간 복잡도: O(N)
+공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-		
-		for(char chr: myString.toCharArray()) {
-			if(chr < 'l') {
-				answer += Character.toString('l');
-			} else {
-				answer += Character.toString(chr);
-			}
-		}
-		
-		return answer;
+        StringBuilder answer = new StringBuilder(myString.length());
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            
+            if(c < 'l') {
+                answer.append('l');
+            } else {
+                answer.append(c);
+            }
+        }
+        
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : myString.toCharArray()) {
+            sb.append((char)Math.max(c, 'l'));
+        }
+        
+        return sb.toString();
+    }
+}
+*/
