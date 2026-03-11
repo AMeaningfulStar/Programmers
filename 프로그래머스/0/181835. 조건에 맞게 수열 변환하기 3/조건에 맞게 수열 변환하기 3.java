9875a4e0a4b08583ae40548bd8ea0856a2486182
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-11 17:18:56 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 13.22 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,12 +1,37 @@
+/*
+알고리즘: 선형 순회 + 조건 분기
+핵심 개념:
+• 배열 순회
+• 홀짝 판별 (% 2)
+• 조건에 따라 연산 수행
+시간 복잡도: O(N)
+공간 복잡도: O(N)
+*/
+
 class Solution {
     public int[] solution(int[] arr, int k) {
-        for(int idx = 0; idx < arr.length; idx++) {
-			if(k % 2 == 0) {
-				arr[idx] += k;
-			} else {
-				arr[idx] *= k;
-			}
-		}
-		return arr;
+        int[] answer = new int[arr.length];
+        boolean isEven = k % 2 == 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            answer[i] = isEven ? arr[i] + k : arr[i] * k;
+        }
+
+        return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i++) {
+            answer[i] = (k % 2 == 0) ? arr[i] + k : arr[i] * k;
+        }
+
+        return answer;
+    }
+}
+*/
