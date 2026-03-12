50378b390501df318fa612e3d28ac2355332b9ba
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-12 11:28:33 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.06 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,11 +1,21 @@
+/*
+알고리즘: 대각선 인덱스 직접 접근
+핵심 개념:
+• 2차원 배열 생성
+• (i,i) 위치는 대각선
+• 대각선 원소만 값 설정
+시간 복잡도: O(n)
+공간 복잡도: O(n²)
+*/
+
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-		
-		for(int idx = 0; idx < n; idx++) {
-			answer[idx][idx] = 1;
-		}
-		
-		return answer;
+        
+        for(int idx = 0; idx < n; idx++) {
+            answer[idx][idx] = 1;
+        }
+        
+        return answer;
     }
 }
