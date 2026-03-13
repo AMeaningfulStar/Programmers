0ee92bdc612b0299633c5c3619a4a167a86a8d9f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-13 13:26:33 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.16 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,13 +1,25 @@
+/*
+알고리즘: 대칭 행렬 검사 (Matrix Symmetry Check)
+핵심 개념:
+• (i, j) 와 (j, i) 비교
+• 대각선 기준 대칭 검사
+• 불일치 발견 시 즉시 종료
+시간 복잡도: O(n² / 2)
+공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[][] arr) {
-        for(int x_idx = 0; x_idx < arr.length; x_idx++) {
-            for(int y_idx = 0; y_idx < arr.length; y_idx++) {
-                if(arr[x_idx][y_idx] != arr[y_idx][x_idx]) {
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i][j] != arr[j][i]) {
                     return 0;
                 }
             }
         }
-        
+
         return 1;
     }
 }
