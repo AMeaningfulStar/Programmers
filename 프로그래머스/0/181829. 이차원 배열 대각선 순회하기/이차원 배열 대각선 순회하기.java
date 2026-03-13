0503670829306f47cad884662dfb65074efbdcd0
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-13 13:45:15 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.06 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.java
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.java
@@ -1,13 +1,25 @@
+/*
+알고리즘: 2차원 배열 선형 순회 + 조건 필터링
+핵심 개념:
+• 2중 반복문으로 모든 좌표 순회
+• 좌표 합 i + j를 기준으로 조건 판별
+• 조건을 만족하는 값만 누적합 계산
+시간 복잡도: O(R × C) (R = 행 개수, C = 열 개수)
+공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[][] board, int k) {
         int answer = 0;
-        for(int i =0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++) {
-                if(i+j <= k) {
-                    answer += board[i][j];
-                }
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols && j <= k - i; j++) {
+                answer += board[i][j];
             }
         }
+
         return answer;
     }
 }
