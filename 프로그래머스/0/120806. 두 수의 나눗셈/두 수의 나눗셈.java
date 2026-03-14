d65d4487ac8afad14361e9cb0240400df3123d04
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-14 12:39:13 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,5 +1,15 @@
+/*
+알고리즘: 산술 연산 (Arithmetic Calculation)
+핵심 개념:
+• 정수 → 실수 캐스팅
+• 실수 나눗셈
+• 소수점 버림 (type casting)
+시간 복잡도: O(1)
+공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int num1, int num2) {
-        return (int) (((double) num1 / num2) * 1000);
+        return (int) ((double) num1 / num2 * 1000);
     }
 }
