0817d55cf50bfe189e1c10172c5acbb911adf83d
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-16 23:35:06 +0900

    [level 0] Title: 나머지 구하기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,3 +1,11 @@
+/*
+알고리즘: 산술 연산 (Arithmetic Calculation)
+핵심 개념:
+• 나머지 연산자 %
+시간 복잡도: O(1)
+공간 복잡도: O(1)
+*/
+    
 class Solution {
     public int solution(int num1, int num2) {
         return num1 % num2;
