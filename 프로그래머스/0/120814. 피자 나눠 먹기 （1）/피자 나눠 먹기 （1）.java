3d9aaecb1f2c1b2e9df8bb5ea7c7221cb5342795
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-17 10:34:21 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,15 +1,25 @@
+/*
+• 알고리즘: 산술 연산 (Arithmetic Calculation)
+• 핵심 개념
+ • 정수 나눗셈을 이용한 몫 계산
+ • 나머지를 이용한 추가 필요 여부 판단
+ • 올림 처리 로직 ((n + divisor - 1) / divisor)
+• 시간 복잡도: O(1)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        
-        while(true) {
-            if((7 * answer) / n >= 1) {
-                break;
-            }
-            
-            answer++;
-        }
-        
-        return answer;
+        int answer = n / 7;
+        return n % 7 > 0 ? answer + 1 : answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int n) {
+        return (n + 6) / 7;
+    }
+}
+*/
