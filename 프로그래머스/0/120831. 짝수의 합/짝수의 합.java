92fb0da9c166b6b3b33ebb44f107a23605692a85
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-17 10:03:48 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,11 +1,31 @@
+/*
+• 알고리즘: 반복문 기반 누적 (Iteration & Accumulation)
+• 핵심 개념
+ • 일정 간격 반복 (num += 2)
+ • 짝수만 선택적으로 순회
+ • 누적 합 계산
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int num = 2; num <= n; num += 2){
+        for(int num = 0; num <= n; num += 2) {
             answer += num;
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int n) {
+        int k = n / 2;
+        return k * (k + 1);
+    }
+}
+*/
