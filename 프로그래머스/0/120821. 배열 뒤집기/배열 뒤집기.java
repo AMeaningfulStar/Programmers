6a8c9b14ef8ee68ac908e554e6711508a251089f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-17 10:22:02 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,3 +1,13 @@
+/*
+• 알고리즘: 배열 순회 (Array Traversal)
+• 핵심 개념
+ • 인덱스를 이용한 역순 접근
+ • 새로운 배열에 값 재배치
+ • length - idx - 1 인덱스 계산
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
@@ -9,3 +19,24 @@ class Solution {
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int[] solution(int[] num_list) {
+        int left = 0;
+        int right = num_list.length - 1;
+
+        while(left < right) {
+            int temp = num_list[left];
+            num_list[left] = num_list[right];
+            num_list[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return num_list;
+    }
+}
+*/
