a432ccfdf1a29bfc4547b0d39203d2648b99ad96
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-17 10:48:34 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,3 +1,13 @@
+/*
+• 알고리즘: 배열 순회 (Array Traversal)
+• 핵심 개념
+ • 배열 전체 순회
+ • 조건 비교 (> height)
+ • 조건 만족 개수 카운팅
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
@@ -11,3 +21,16 @@ class Solution {
         return answer;
     }
 }
+
+/*
+다른 풀이
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        return (int) Arrays.stream(array)
+                           .filter(num -> num > height)
+                           .count();
+    }
+}
+*/
