9aa8d5fdc79edac0d7d8eed83840c09d5ff000b6
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-17 10:25:16 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,11 +1,30 @@
+/*
+• 알고리즘: 문자열 처리 (String Manipulation)
+• 핵심 개념
+ • 문자열 역순 순회
+ • StringBuilder를 이용한 문자열 생성
+ • charAt()을 통한 문자 접근
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder(my_string.length());
         
-        for(int idx = my_string.length() - 1; idx >= 0; idx--) {
-            answer += my_string.split("")[idx];
+        for(int i = my_string.length() - 1; i >= 0; i--) {
+            answer.append(my_string.charAt(i));
         }
         
-        return answer;
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String my_string) {
+        return new StringBuilder(my_string).reverse().toString();
+    }
+}
+*/
