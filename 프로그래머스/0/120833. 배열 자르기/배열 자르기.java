5ecc8d4702d4c4ca158640d4d3d1dba7e2566762
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-18 13:33:18 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,9 +1,32 @@
+/*
+• 알고리즘: 배열 슬라이싱 (Array Slicing)
+• 핵심 개념
+ • 특정 구간 추출
+ • 시작 인덱스 ~ 끝 인덱스 접근
+ • Java 배열 복사 (copyOfRange)
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+    }
+}
+
+/*
+다른 풀이
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = new int[num2 - num1 + 1];
         
-        System.arraycopy(numbers, num1, answer, 0, num2 - num1 + 1);
+        for(int i = num1; i <= num2; i++) {
+            answer[i - num1] = numbers[i];
+        }
         
         return answer;
     }
 }
+*/
