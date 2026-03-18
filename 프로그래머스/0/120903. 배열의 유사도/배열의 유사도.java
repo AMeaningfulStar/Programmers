ee21e1c55b2ac1e46bdea398a49f8910ff5f7fce
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-18 13:51:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.10 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,15 +1,45 @@
+/*
+• 알고리즘: 해시 기반 탐색 (Hash-based Search)
+• 핵심 개념
+ • 공통 요소 찾기
+ • HashSet을 이용한 빠른 탐색 (O(1))
+ • contains 연산 최적화
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
+import java.util.*;
+
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
         
-        for(int s1_idx = 0; s1_idx < s1.length; s1_idx++) {
-            for(int s2_idx = 0; s2_idx < s2.length; s2_idx++) {
-                if(s1[s1_idx].equals(s2[s2_idx])) {
-                    answer++;
-                }
+        for(String str: s1) {
+            if(Arrays.asList(s2).contains(str)) {
+                answer += 1;
             }
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        Set<String> set = new HashSet<>(Arrays.asList(s2));
+        int answer = 0;
+        
+        for(String str : s1) {
+            if(set.contains(str)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
+*/
