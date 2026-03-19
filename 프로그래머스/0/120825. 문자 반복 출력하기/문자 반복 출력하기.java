44e726222d9afec22685f9ab4d6b0f90a45d3917
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-19 11:51:00 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,13 +1,36 @@
+/*
+• 알고리즘: 문자열 생성 + 반복 (String Construction & Repetition)
+• 핵심 개념
+ • 문자열 순회 (toCharArray())
+ • 문자 반복
+ • StringBuilder 활용 (성능 최적화)
+• 시간 복잡도: O(N × n)
+• 공간 복잡도: O(N × n)
+*/
+
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
-        for(String str:my_string.split("")) {
-            for(int turn = 0; turn < n; turn++) {
-                answer += str;
+        for(char c: my_string.toCharArray()) {
+            for(int i = 0; i < n; i++){
+                answer.append(c);
             }
         }
         
-        return answer;
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.chars()
+                        .mapToObj(c -> String.valueOf((char)c).repeat(n))
+                        .collect(Collectors.joining());
+    }
+}
+*/
