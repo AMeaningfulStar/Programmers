e49b61793fdbc773029cb480a3ef864a10ae8395
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-19 11:36:40 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,11 +1,34 @@
+/*
+• 알고리즘: 배열 순회 + 변환 (Array Traversal & Transformation)
+• 핵심 개념
+ • 배열 인덱스 순회
+ • 문자열 길이 추출 (length())
+ • 결과 배열에 값 매핑
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
         
-        for(int idx = 0; idx < strlist.length; idx++){
-            answer[idx] = strlist[idx].length();
+        for(int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        return Arrays.stream(strlist)
+                     .mapToInt(String::length)
+                     .toArray();
+    }
+}
+*/
