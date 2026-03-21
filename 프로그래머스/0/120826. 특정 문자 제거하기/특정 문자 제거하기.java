c078118756d255d6d53e9ba49bb0a39632b5b141
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-21 13:59:01 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,13 +1,32 @@
+/*
+• 알고리즘: 문자열 처리 (String Manipulation)
+• 핵심 개념
+ • 특정 문자열 치환 (replace)
+ • 문자열 탐색 및 제거
+ • 불필요 문자 필터링
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String my_string, String letter) {
-        String answer = "";
-        
-        for(String str:my_string.split("")) {
-            if(!str.equals(letter)) {
-                answer += str;
-            }
-        }
-        
-        return answer;
+        return my_string.replace(letter, "");
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : my_string.toCharArray()) {
+            if(!String.valueOf(c).equals(letter)) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
+*/
