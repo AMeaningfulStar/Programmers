e6664c958a5ba6b47e143d871f12b0ca3e7539f3
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-21 14:42:09 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.78 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,15 +1,44 @@
+/*
+• 알고리즘: 문자열 변환 (String Transformation)
+• 핵심 개념
+ • 문자 순회
+ • 대소문자 판별 (Character.isUpperCase)
+ • 문자 변환 (toLowerCase, toUpperCase)
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder(my_string.length());
         
-        for(int idx = 0; idx < my_string.length(); idx++) {
-            if(my_string.split("")[idx].matches("^[A-Z]+$")) {
-                answer += my_string.split("")[idx].toLowerCase();
+        for(int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if(Character.isUpperCase(c)) {
+                answer.append(Character.toLowerCase(c));
             } else {
-                answer += my_string.split("")[idx].toUpperCase();
+                answer.append(Character.toUpperCase(c));
             }
         }
         
-        return answer;
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : my_string.toCharArray()) {
+            sb.append(Character.isUpperCase(c) 
+                      ? Character.toLowerCase(c) 
+                      : Character.toUpperCase(c));
+        }
+        
+        return sb.toString();
+    }
+}
+*/
