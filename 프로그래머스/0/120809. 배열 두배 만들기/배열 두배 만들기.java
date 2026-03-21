85a0a0329e351b20a843d0b08377b4f438985695
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-21 14:27:44 +0900

    [level 0] Title: 배열 두배 만들기, Time: 3.16 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,9 +1,34 @@
+/*
+• 알고리즘: 배열 순회 + 변환 (Array Traversal & Transformation)
+• 핵심 개념
+ • 배열 요소 순회
+ • 각 요소 값 변환 (num * 2)
+ • map을 이용한 데이터 변환
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        for(int idx = 0; idx < numbers.length; idx++) {
-            numbers[idx] *= 2;
-        }
-        
-        return numbers;
+        return Arrays.stream(numbers)
+                     .map(num -> num * 2)
+                     .toArray();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i] * 2;
+        }
+        
+        return answer;
+    }
+}
+*/
