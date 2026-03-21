0d12ff7d763b9d7ddc787ebe772aacca0349ef1d
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-21 14:01:56 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,13 +1,45 @@
+/*
+• 알고리즘: 문자열 처리 (String Manipulation)
+• 핵심 개념
+ • 특정 문자 집합 제거
+ • 문자열 치환 (replace, replaceAll)
+ • 문자 필터링
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        
-        for(String str: my_string.split("")) {
-            if(!str.equals("a") && !str.equals("e") && !str.equals("i") && !str.equals("o") && !str.equals("u")) {
-                answer += str;
-            }
-        }
-        
+        String answer = my_string.replace("a" ,  "")
+                                 .replace("e" ,  "")
+                                 .replace("i" ,  "")
+                                 .replace("o" ,  "")
+                                 .replace("u" ,  "");
         return answer;
     }
 }
+
+/*
+다른 풀이
+1.
+class Solution {
+    public String solution(String my_string) {
+        return my_string.replaceAll("[aeiou]", "");
+    }
+}
+
+2.
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : my_string.toCharArray()) {
+            if("aeiou".indexOf(c) == -1) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
+*/
