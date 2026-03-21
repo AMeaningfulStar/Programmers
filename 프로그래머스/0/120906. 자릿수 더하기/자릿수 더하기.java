59c72904ae41c31dd627f7633de75b55d4d4a978
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-21 14:10:22 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,12 +1,38 @@
+/*
+• 알고리즘: 수학 연산 + 반복 (Arithmetic & Iteration)
+• 핵심 개념
+ • 자릿수 분해 (% 10, / 10)
+ • 반복을 통한 누적 합 계산
+ • 또는 문자열 순회 방식
+• 시간 복잡도: O(log N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String[] num_list = Integer.toString(n).split("");
+        String str = String.valueOf(n);
         
-        for(String num: num_list) {
-            answer += Integer.parseInt(num);
+        for(int i = 0; i < str.length(); i++) {
+            answer += str.charAt(i) - '0';
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        
+        return answer;
+    }
+}
+*/
