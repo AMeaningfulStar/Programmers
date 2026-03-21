ea7e73ebacb6564b3b627b403b9f05e3586493ec
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-21 14:04:59 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,8 +1,18 @@
+/*
+• 알고리즘: 배열 순회 + 카운팅 (Array Traversal & Counting)
+• 핵심 개념
+ • 배열 전체 순회
+ • 짝수/홀수 판별 (num % 2)
+ • 조건에 따른 카운트 증가
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         
-        for(int num:num_list) {
+        for(int num: num_list) {
             if(num % 2 == 0) {
                 answer[0] += 1;
             } else {
@@ -13,3 +23,18 @@ class Solution {
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        for(int num : num_list) {
+            answer[num % 2 == 0 ? 0 : 1]++;
+        }
+        
+        return answer;
+    }
+}
+*/
