687386ec0f03054cb48775738422c16b0eb458b5
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-22 14:05:09 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,13 +1,42 @@
+/*
+• 알고리즘: 배열 순회 + 카운팅 (Array Traversal & Counting)
+• 핵심 개념
+ • 배열 전체 순회
+ • 조건 비교 (num == n)
+ • 조건 만족 개수 증가
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
         
         for(int num: array) {
-            if(num == n) {
-                answer += 1;
-            }
+            if(num == n) answer += 1;    
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+1.
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        return (int) Arrays.stream(array)
+                           .filter(num -> num == n)
+                           .count();
+    }
+}
+
+2.
+Map<Integer, Integer> map = new HashMap<>();
+
+for(int num : array) {
+    map.put(num, map.getOrDefault(num, 0) + 1);
+}
+*/
