116a936590a241443c65ec57385754f526a90fa1
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-22 14:37:30 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,21 +1,46 @@
+/*
+• 알고리즘: 그리디 (Greedy Algorithm)
+• 핵심 개념
+ • 큰 단위부터 선택
+ • 몫과 나머지 활용
+ • 최적해 보장 구조
+• 시간 복잡도: O(1)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int hp) {
         int answer = 0;
         
-        if((hp / 5) > 0 ) {
-            answer += hp / 5;
-            hp = hp % 5;
-        }
+        answer += hp / 5;
+        hp = hp % 5;
         
-        if((hp / 3) > 0 ) {
-            answer += hp / 3;
-            hp = hp % 3;
-        }
+        answer += hp / 3;
+        hp = hp % 3;
         
-        if(hp > 0 ) {
-            answer += hp;
-        }
+        answer += hp / 1;
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+1.
+class Solution {
+    public int solution(int hp) {
+        return hp / 5 + (hp % 5) / 3 + (hp % 5 % 3);
+    }
+}
+
+2.
+class Solution {
+    public int solution(int hp) {
+        int general = hp / 5;
+        int soldier = (hp % 5) / 3;
+        int worker = hp % 5 % 3;
+
+        return general + soldier + worker;
+    }
+}
+*/
