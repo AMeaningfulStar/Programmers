18551df678e5224bd75488ad45ed4c9fe338ba6c
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-22 14:32:29 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 188.28 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -1,16 +1,48 @@
+/*
+• 알고리즘: 반복문 + 출력 (Iteration & Output)
+• 핵심 개념
+ • 이중 반복문
+ • 패턴 출력
+ • 문자열 누적 후 출력
+• 시간 복잡도: O(N²)
+• 공간 복잡도: O(N)
+*/
+
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        
+        StringBuilder sb = new StringBuilder();
+        
         for(int line = 0; line < n; line++) {
-            String star = "";
-            for(int count = 0; count < line + 1; count++){
-                star += "*";
+            for(int count = 0; count < line + 1; count++) {
+                sb.append("*");
             }
-            System.out.println(star);
+            
+            System.out.println(sb.toString());
+            
+            sb.setLength(0);
         }
+        
+        /*
+        다른 풀이
+        1.
+        for(int i = 1; i <= n; i++) {
+            System.out.println("*".repeat(i));
+        }
+        
+        2.
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+        
+        sc.close();
     }
 }
