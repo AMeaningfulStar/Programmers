387ff00f020fc6961367f1874b475c13558acba4
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-23 19:57:07 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,5 +1,29 @@
+/*
+• 알고리즘: 수학 계산 (Mathematical Calculation)
+• 핵심 개념
+ • 공간 분할
+ • 각 축별 개수 계산 (size / n)
+ • 곱셈으로 전체 개수 계산
+• 시간 복잡도: O(1)
+• 공간 복잡도: O(1)
+*/
+
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 1;
+        
+        for(int size: box) {
+            answer *= (size / n);
+        }
+        return answer;
+    }
+}
+
+/*
+다른 풀이
 class Solution {
     public int solution(int[] box, int n) {
         return (box[0] / n) * (box[1] / n) * (box[2] / n);
     }
 }
+*/
