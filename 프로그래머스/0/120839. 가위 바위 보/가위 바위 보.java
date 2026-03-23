3da010ee1b543482a7387f23531d95f43dad0cc7
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-23 19:54:06 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,11 +1,42 @@
+/*
+• 알고리즘: 문자열 변환 (String Transformation)
+• 핵심 개념
+ • 문자 순회 (toCharArray)
+ • 조건 기반 변환
+ • 매핑 로직 구현
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
-        for(String str: rsp.split("")) {
-            answer +=  str.equals("2") ? "0" : str.equals("0") ? "5" : "2";
+        for(char c : rsp.toCharArray()) {
+            if(c == '2') {
+                answer.append('0');
+            } else if(c == '0') {
+                answer.append('5');
+            } else {
+                answer.append('2');
+            }
         }
         
-        return answer;
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : rsp.toCharArray()) {
+            sb.append(c == '2' ? '0' : c == '0' ? '5' : '2');
+        }
+        
+        return sb.toString();
+    }
+}
+*/
