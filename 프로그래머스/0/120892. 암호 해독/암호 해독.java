652c13298ec0d5f7a0fb491c8c2ccece68825178
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-23 20:32:26 +0900

    [level 0] Title: 암호 해독, Time: 0.12 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,37 @@
+/*
+• 알고리즘: 반복문 + 간격 이동 (Step Iteration)
+• 핵심 개념
+ • 인덱스 간격 이동 (i += code)
+ • 시작 위치 보정 (code - 1)
+ • 문자열 순회
+• 시간 복잡도: O(N / code) ≈ O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(String cipher, int code) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        char[] c_arr = cipher.toCharArray();
         
-        for(int idx = code - 1; idx < cipher.length(); idx += code) {
-            answer += cipher.split("")[idx];
+        for(int i = code - 1; i < c_arr.length; i += code) {
+            answer.append(c_arr[i]);
         }
         
-        return answer;
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = code - 1; i < cipher.length(); i += code) {
+            sb.append(cipher.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
+*/
