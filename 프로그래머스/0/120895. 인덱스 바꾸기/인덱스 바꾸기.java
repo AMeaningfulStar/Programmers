8820784f06408a19bb4fdeeb9700c279ed61398f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-24 15:23:22 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,18 +1,38 @@
-import java.util.*;
+/*
+• 알고리즘: 문자열 인덱스 교환 (Index Swap)
+• 핵심 개념
+ • 문자 배열 변환 (toCharArray)
+ • swap (임시 변수)
+ • 문자열 재구성
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
 
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String[] str_arr = my_string.split("");
-        String str = str_arr[num1];
-        str_arr[num1] = str_arr[num2];
-        str_arr[num2] = str;
+        char[] arr = my_string.toCharArray();
         
-        String answer = "";
+        char temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
         
-        for(String item: str_arr) {
-            answer += item;
-        }
-        
-        return answer;
+        return new String(arr);
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        char c1 = sb.charAt(num1);
+        char c2 = sb.charAt(num2);
+        
+        sb.setCharAt(num1, c2);
+        sb.setCharAt(num2, c1);
+        
+        return sb.toString();
+    }
+}
+*/
