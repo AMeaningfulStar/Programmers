37928f913b1c2a09b64d01aaa0a669e6b8f9e20f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-24 15:31:41 +0900

    [level 0] Title: 약수 구하기, Time: 4.25 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,21 +1,49 @@
+/*
+• 알고리즘: 약수 탐색 (Divisor Search)
+• 핵심 개념
+ • 나눗셈 (n % i == 0)
+ • 약수 쌍 개념
+ • √n 최적화
+• 시간 복잡도: O(√N)
+• 공간 복잡도: O(N)
+*/
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> num_list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        for(int num = 1; num <= n; num++) {
-            if(n % num == 0) {
-                num_list.add(num);
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                list.add(i);
             }
         }
         
-        int[] answer = new int[num_list.size()];
-        
-        for(int idx = 0; idx < num_list.size(); idx++) {
-            answer[idx] = num_list.get(idx);
-        }
-    
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
+
+/*
+다른 풀이
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                list.add(i);
+                if(i != n / i) {
+                    list.add(n / i);
+                }
+            }
+        }
+        
+        Collections.sort(list);
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
+*/
