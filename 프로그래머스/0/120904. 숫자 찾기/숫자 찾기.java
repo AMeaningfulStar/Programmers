26d1217bd2d9ee431052e570271f93160b92c32c
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-24 15:48:22 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,35 @@
+/*
+• 알고리즘: 문자열 탐색 (String Search)
+• 핵심 개념
+ • 문자열 변환 (String.valueOf)
+ • indexOf 활용
+ • char 비교 (k + '0')
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int solution(int num, int k) {
-        String[] num_strs = Integer.toString(num).split("");
+        String str = String.valueOf(num);
+        int idx = str.indexOf(String.valueOf(k));
         
-        for(int idx = 0; idx < num_strs.length; idx++){
-            if(num_strs[idx].equals(Integer.toString(k))) {
-                return idx + 1;
+        return idx == -1 ? -1 : idx + 1;
+    }
+}
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int num, int k) {
+        char[] arr = String.valueOf(num).toCharArray();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == (char)(k + '0')) {
+                return i + 1;
             }
         }
         
         return -1;
     }
 }
+*/
