4418700d70aefb78377de3a7f3729eb5743b1e0f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-25 13:51:11 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,15 +1,41 @@
+/*
+• 알고리즘: 최대공약수(GCD) / 최소공배수(LCM)
+• 핵심 개념
+ • GCD (유클리드 호제법)
+ • LCM = (a × b) / GCD
+ • 분배 문제 → 공배수 문제
+• 시간 복잡도: O(log N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int n) {
-        int answer = 1;
+        int answer = 0;
         
         while(true) {
-            if((6 * answer) % n == 0) {
-                break;
-            }
+            answer += 1;
             
-            answer++;
+            if((6 * answer) % n == 0) break;
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int n) {
+        return n / gcd(n, 6);
+    }
+    
+    private int gcd(int a, int b) {
+        while(b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
+*/
