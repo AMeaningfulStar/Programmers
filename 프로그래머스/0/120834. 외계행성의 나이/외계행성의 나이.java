c880f72561d3cd005fb23bd533d74723959716c1
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-25 13:56:44 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,12 +1,37 @@
+/*
+• 알고리즘: 문자 변환 (Character Mapping)
+• 핵심 개념
+ • ASCII 연산
+ • 문자 → 숫자 (c - '0')
+ • 숫자 → 문자 (+ 'a')
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public String solution(int age) {
-        String answer = "";
-        String age962 = "abcdefghij";
-        String[] ageArr = String.valueOf(age).split("");
+        StringBuilder answer = new StringBuilder();
         
-        for(int i = 0; i < ageArr.length; i++) {
-            answer += age962.charAt(Integer.valueOf(ageArr[i]));
+        for(char c : String.valueOf(age).toCharArray()) {
+            answer.append((char)(c - '0' + 'a'));
         }
-        return answer;
+        
+        return answer.toString();
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public String solution(int age) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(age > 0) {
+            sb.append((char)('a' + (age % 10)));
+            age /= 10;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
+*/
