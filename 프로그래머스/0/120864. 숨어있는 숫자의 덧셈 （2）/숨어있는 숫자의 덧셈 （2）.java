1a91f6f629670c04c125c17a6bde037c82bdde0f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-25 19:58:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,24 +1,54 @@
+/*
+• 알고리즘: 문자열 파싱 + 숫자 누적
+• 핵심 개념
+ • 숫자 판별 (Character.isDigit)
+ • 문자열 누적 (StringBuilder)
+ • 경계 처리 (마지막 숫자)
+ • 정규식 분리 (split)
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        String num = "";  // 누적 숫자 문자열
-
-        for (String str : my_string.split("")) {
-            if (str.matches("[0-9]")) {  // 한 글자가 숫자인지 확인
-                num += str;
+        StringBuilder num = new StringBuilder();
+        
+        for(char c : my_string.toCharArray()) {
+            if(Character.isDigit(c)) {
+                num.append(c);
             } else {
-                if (!num.isEmpty()) {
-                    answer += Integer.parseInt(num);
-                    num = "";
+                if(num.length() > 0) {
+                    answer += Integer.parseInt(num.toString());
+                    num.setLength(0);
                 }
             }
         }
-
-        // 마지막에 숫자가 남아있다면 처리
-        if (!num.isEmpty()) {
-            answer += Integer.parseInt(num);
+        
+        // 마지막 숫자 처리
+        if(num.length() > 0) {
+            answer += Integer.parseInt(num.toString());
         }
-
+        
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String[] arr = my_string.split("[^0-9]+");
+        
+        for(String s : arr) {
+            if(!s.isEmpty()) {
+                answer += Integer.parseInt(s);
+            }
+        }
+        
+        return answer;
+    }
+}
+*/
