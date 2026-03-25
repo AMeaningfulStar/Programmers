171491f7694e85f7458adfc4f86d66e3ab2cb14b
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-25 19:47:34 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,18 +1,44 @@
+/*
+• 알고리즘: 반복문 + 누적 곱 (Factorial Iteration)
+• 핵심 개념
+ • 누적 곱 (factorial *= i)
+ • 종료 조건 (> n)
+ • 경계 처리 (-1)
+• 시간 복잡도: O(k) (k는 정답 값)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int num = 1;
-        int value = 1;
+        int answer = 1;
+        int total = 1;
         
         while(true) {
-            num *= value;
-            if(num > n) {
-                answer = value - 1;
+            if(n < total * answer) {
                 break;
             }
-            value++;
+            
+            total *= answer;
+            answer++;
         }
         
-        return answer;
+        return answer - 1;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int factorial = 1;
+        
+        while(factorial <= n) {
+            i++;
+            factorial *= i;
+        }
+        
+        return i - 1;
+    }
+}
+*/
