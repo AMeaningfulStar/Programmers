d2ff715cb6f6886fe016fb240d2aba99c1019571
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 10:23:26 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.20 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,16 +1,53 @@
+/*
+• 알고리즘: 문자열 파싱 + 누적 계산
+• 핵심 개념
+ • 공백 기준 문자열 분리 (split)
+ • 숫자 누적 합 계산
+ • "Z" 등장 시 직전 숫자 취소
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+(토큰 배열 저장 기준)
+*/
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
         String[] s_arr = s.split(" ");
         
-        for(int idx = 0; idx < s_arr.length; idx++){
-            if(s_arr[idx].equals("Z") && (idx - 1) >= 0) {
-                answer -= Integer.parseInt(s_arr[idx - 1]);
+        for(int i = 0; i < s_arr.length; i++) {
+            if(s_arr[i].equals("Z")) {
+                answer -= Integer.parseInt(s_arr[i - 1]);
             } else {
-                answer += Integer.parseInt(s_arr[idx]);
+                 answer += Integer.parseInt(s_arr[i]);
             }
         }
         
         return answer;
     }
 }
+
+/*
+다른 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String token : s.split(" ")) {
+            if (token.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        int answer = 0;
+        for (int num : stack) {
+            answer += num;
+        }
+        
+        return answer;
+    }
+}
+*/
