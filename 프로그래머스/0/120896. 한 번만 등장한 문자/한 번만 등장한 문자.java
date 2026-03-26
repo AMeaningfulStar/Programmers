775c38f50f2f43e697fe3ca6bf84a57faffb485e
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 09:54:37 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.43 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,28 +1,61 @@
+/*
+• 알고리즘: 빈도수 카운팅 + 정렬
+• 핵심 개념
+ • Map / 배열 카운팅
+ • 조건 필터링 (count == 1)
+ • 정렬
+• 시간 복잡도: O(N log N)
+• 공간 복잡도: O(N)
+*/
+
 import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String[] str_arr = s.split("");
-        String answer = "";
+        Map<Character, Integer> map = new HashMap<>();
         
-        Arrays.sort(str_arr);
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
         
-        for(int idx = 0; idx < str_arr.length; idx++) {
-            int count = 0;
-            
-            for(String str:str_arr) {
-                if(str_arr[idx].equals(str)) {
-                    count++;
-                }
-            }
-            
-            if(count == 1) {
-                answer += str_arr[idx];
+        List<Character> list = new ArrayList<>();
+        
+        for(char c : map.keySet()) {
+            if(map.get(c) == 1) {
+                list.add(c);
             }
         }
         
+        Collections.sort(list);
         
+        StringBuilder answer = new StringBuilder();
         
-        return answer;
+        for(char c : list) {
+            answer.append(c);
+        }
+        
+        return answer.toString();
     }
 }
+
+/*
+class Solution {
+    public String solution(String s) {
+        int[] count = new int[26];
+        
+        for(char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i = 0; i < 26; i++) {
+            if(count[i] == 1) {
+                answer.append((char)(i + 'a'));
+            }
+        }
+        
+        return answer.toString();
+    }
+}
+*/
