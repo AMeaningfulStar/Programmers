057c134a2990b95b903f7e414627bc52ab8e4ee3
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 13:50:46 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.23 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,14 +1,28 @@
+/*
+• 알고리즘: 문자열 파싱 + 누적 연산 (Sequential Evaluation)
+• 핵심 개념
+ • 문자열 분리 (split)
+ • 토큰 순회
+ • 누적 계산 (accumulator)
+ • 연산자 분기 처리
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int solution(String my_string) {
-        String[] s = my_string.split(" ");
-        int answer = Integer.parseInt(s[0]);
-        for (int i = 1; i < s.length - 1; i += 2) {
-            if (s[i].equals("+")) {
-                answer += Integer.parseInt(s[i + 1]);
+        String[] arr = my_string.split(" ");
+        
+        int answer = Integer.parseInt(arr[0]);
+        
+        for (int i = 1; i < arr.length; i += 2) {
+            if (arr[i].equals("+")) {
+                answer += Integer.parseInt(arr[i + 1]);
             } else {
-                answer -= Integer.parseInt(s[i + 1]);
+                answer -= Integer.parseInt(arr[i + 1]);
             }
         }
+        
         return answer;
     }
 }
