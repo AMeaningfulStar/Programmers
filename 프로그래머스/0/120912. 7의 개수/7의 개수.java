11647fa347df2a19a7198c149ea67e5de85376fc
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 10:09:19 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,21 +1,43 @@
+/*
+• 알고리즘: 자리수 분해 (Digit Decomposition)
+• 핵심 개념
+ • % 10 → 마지막 자리
+ • / 10 → 자리 이동
+ • 반복 → 전체 자리 탐색
+• 시간 복잡도: O(N × 자릿수)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-		
-		for(int idx = 0; idx < array.length; idx++) {
-			String num = Integer.toString(array[idx]);
-			
-			if(num.contains("7")) {
-				String[] arr = num.split("");
-				
-				for(String str: arr) {
-					if(str.equals("7")) {
-						answer += 1;
-					}
-				}
-			}
-		}
-		
-		return answer;
+        
+        for(int num:array) {
+            int temp = num;
+            
+            while(temp > 0) {
+                if(temp % 10 == 7) answer++;
+                temp /= 10;
+            }
+        }
+        
+        return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int num : array) {
+            for(char c : String.valueOf(num).toCharArray()) {
+                if(c == '7') answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
+*/
