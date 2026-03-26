fd58f2e4447b7a8344c0da520875a9093110bc36
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 09:08:58 +0900

    [level 0] Title: k의 개수, Time: 14.10 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,15 +1,44 @@
+/*
+• 알고리즘: 완전탐색 + 자리수 분해
+• 핵심 개념
+ • 숫자 → 문자열 or 자릿수 분해
+ • 각 자리 비교
+ • 누적 카운트
+• 시간 복잡도: O(N × 자릿수)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        String kStr = Integer.toString(k);
-
-        for (int num = i; num <= j; num++) {
-            String[] digits = Integer.toString(num).split("");
-            for (String digit : digits) {
-                if (digit.equals(kStr)) answer++;
+        char target = (char)(k + '0');
+        
+        for(int num = i; num <= j; num++) {
+            for(char c : String.valueOf(num).toCharArray()) {
+                if(c == target) answer++;
             }
         }
-
+        
         return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        
+        for(int num = i; num <= j; num++) {
+            int temp = num;
+            
+            while(temp > 0) {
+                if(temp % 10 == k) answer++;
+                temp /= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
+*/
