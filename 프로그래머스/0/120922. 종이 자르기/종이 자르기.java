e4037e6c9f78c5609bdc745f79c1d744f3034843
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 20:56:45 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,3 +1,13 @@
+/*
+• 알고리즘: 수학적 사고 (Mathematical Insight)
+• 핵심 개념
+ • 조각 수 증가 패턴 (+1씩 증가)
+ • 전체 조각 수 = M × N
+ • 최소 연산 횟수 = (조각 수 - 1)
+• 시간 복잡도: O(1)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int M, int N) {
         return M * N - 1;
