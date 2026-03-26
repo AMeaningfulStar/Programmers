4f586b7e6cbb7dfc2b5f22a594d854902e21d630
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-27 00:41:46 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,14 +1,34 @@
+/*
+• 알고리즘: 시뮬레이션 + 수학적 최적화
+• 핵심 개념
+ • 반복 상태 갱신
+ • 몫/나머지 활용
+ • 쿠폰 재사용 구조
+ • 수학적 패턴 도출 ((n-1)/9)
+• 시간 복잡도:
+ • 시뮬레이션: O(log N)
+ • 수학: O(1)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int chicken) {
-        int service = 0;
-        int coupon = chicken;
-
-        while (coupon >= 10) {
-            int newChicken = coupon / 10;
-            service += newChicken;
-            coupon = coupon % 10 + newChicken; // 남은 쿠폰 + 새로 받은 쿠폰
+        int answer = 0;
+        
+        while(chicken > 9) {
+            answer += chicken / 10;
+            chicken = (chicken / 10) + (chicken % 10);
         }
-
-        return service;
+        
+        return answer;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int chicken) {
+        return (chicken - 1) / 9;
+    }
+}
+*/
