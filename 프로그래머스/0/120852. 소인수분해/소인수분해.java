6cdb16747944f445622a0a5f82993e6d433dfcb0
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-26 12:31:51 +0900

    [level 0] Title: 소인수분해, Time: 0.19 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,23 +1,69 @@
+/*
+• 알고리즘: 소인수분해 (Prime Factorization)
+• 핵심 개념
+ • 2부터 시작해서 나누어 떨어지는 수 찾기
+ • 나누어 떨어지면 소인수로 추가
+ • 같은 소인수는 while로 모두 제거
+ • 중복 없이 한 번만 저장
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-       ArrayList<Integer> tmpList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        for(int i = 2; i <= n; i++) {
-            if(n % i == 0) {
-                while(n % i == 0) {
-                    n /= i;
+        for (int num = 2; num <= n; num++) {
+            if (n == 1) break;
+            
+            if (n % num == 0) {
+                list.add(num);
+                
+                while (n % num == 0) {
+                    n /= num;
                 }
-                tmpList.add(i);
             }
         }
         
-        int[] answer = new int[tmpList.size()];
-        
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = tmpList.get(i);
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
+
+/*
+다른 풀이
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int num = 2; num * num <= n; num++) {
+            if (n % num == 0) {
+                list.add(num);
+                
+                while (n % num == 0) {
+                    n /= num;
+                }
+            }
+        }
+        
+        if (n > 1) {
+            list.add(n);
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
+*/
