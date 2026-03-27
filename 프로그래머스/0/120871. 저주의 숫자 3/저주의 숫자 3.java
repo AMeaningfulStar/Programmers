a1220ae5c04cd3a58969a5930ee3d80bb544bdff
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-27 15:52:57 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.32 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,18 +1,30 @@
+/*
+• 알고리즘: 시뮬레이션 (Simulation)
+• 핵심 개념
+ • 1부터 차례대로 숫자 증가
+ • 3의 배수 판별 (num % 3 == 0)
+ • 숫자 3 포함 여부 판별 (contains("3"))
+ • 유효한 숫자만 카운트
+• 시간 복잡도: O(N × D)
+ • D는 숫자를 문자열로 바꿨을 때 자릿수 길이
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         int count = 0;
-        int i = 1;
-    
-        while(count != n){
-            if(i % 3 != 0 && i % 10 != 3 && (i / 10) % 10 != 3){
-                answer = i;
-                count++;
+        int num = 0;
+        
+        while (count < n) {
+            num++;
+            
+            if (num % 3 == 0 || String.valueOf(num).contains("3")) {
+                continue;
             }
             
-            i++;
+            count++;
         }
         
-        return answer;
+        return num;
     }
 }
