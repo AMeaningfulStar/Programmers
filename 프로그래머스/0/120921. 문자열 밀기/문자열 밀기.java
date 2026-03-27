10f218a161e2f89e2908fa2b4b7febdb24d7e2aa
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-27 16:25:07 +0900

    [level 0] Title: 문자열 밀기, Time: 0.07 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,15 +1,52 @@
+/*
+• 알고리즘: 문자열 회전 시뮬레이션 (String Rotation Simulation)
+• 핵심 개념
+ • 문자열 비교 (equals)
+ • 오른쪽 한 칸 회전
+ • 반복 시뮬레이션
+ • 문자열 재구성
+• 시간 복잡도: O(N²)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int solution(String A, String B) {
-        int answer = -1;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.equals(B)) return i;
 
-		for (int idx = 0; idx < A.length(); idx++) {
-			if (A.equals(B)) {
-				return idx;
-			}
+            StringBuilder str = new StringBuilder(A.length());
+            str.append(A.charAt(A.length() - 1));
+            str.append(A.substring(0, A.length() - 1));
 
-			A = A.split("")[A.length() - 1] + A.substring(0, A.length() - 1);
-		}
+            A = str.toString();
+        }
 
-		return answer;
+        return -1;
     }
 }
+
+/*
+다른 풀이
+1.
+class Solution {
+    public int solution(String A, String B) {
+        for (int i = 0; i < A.length(); i++) {
+            if (A.equals(B)) {
+                return i;
+            }
+            
+            A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);
+        }
+        
+        return -1;
+    }
+}
+
+2.
+class Solution {
+    public int solution(String A, String B) {
+        int idx = (A + A).indexOf(B);
+        return idx == -1 ? -1 : idx;
+    }
+}
+*/
