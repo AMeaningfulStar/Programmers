e71f8bba4d218669f9eaaa5cfcb8a3e24417452a
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-28 16:42:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,9 +1,44 @@
+/*
+• 알고리즘: 패턴 탐지 (등차 / 등비)
+• 핵심 개념
+ • 차이 비교
+ • 비율 비교
+ • 마지막 값 기반 계산
+• 시간 복잡도: O(1)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[] common) {
-        	if (common[1] + (common[1] - common[0]) == common[2]) {
-			return common[common.length - 1] + (common[1] - common[0]);
-		}else {
-			return common[common.length - 1] * (common[1] / common[0]);
-		}
+        int diff1 = common[1] - common[0];
+        int diff2 = common[2] - common[1];
+        
+        if (diff1 == diff2) {
+            return common[common.length - 1] + diff1;
+        }
+        
+        int ratio = common[1] / common[0];
+        return common[common.length - 1] * ratio;
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int[] common) {
+        int a = common[0];
+        int b = common[1];
+        int c = common[2];
+        
+        // 등차수열
+        if (b - a == c - b) {
+            int diff = b - a;
+            return common[common.length - 1] + diff;
+        }
+        
+        // 등비수열
+        int ratio = b / a;
+        return common[common.length - 1] * ratio;
+    }
+}
+*/
