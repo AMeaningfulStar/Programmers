0c60533a2f3a3ee9d26e2760da70c0a376b30d07
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-28 14:23:19 +0900

    [level 0] Title: 다항식 더하기, Time: 11.50 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,16 +1,45 @@
+/*
+• 알고리즘: 문자열 파싱 + 누적 합 (Parsing & Aggregation)
+• 핵심 개념
+ • 문자열 분리 (split)
+ • 토큰 분류 (x, 숫자, 연산자)
+ • 계수 누적
+ • 조건별 문자열 생성
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public String solution(String polynomial) {
-        int xNum = 0; int num = 0;
-
-        for (String s : polynomial.split(" ")) {
-            if (s.contains("x")) // x가 있으면
-                xNum += s.equals("x") ? 1 : Integer.parseInt(s.replaceAll("x", ""));
-            else if (!s.equals("+"))
-                num += Integer.parseInt(s);
+        int xNum = 0;
+        int num = 0;
+        
+        for (String str : polynomial.split(" ")) {
+            if (str.equals("+")) continue;
+            
+            if (str.contains("x")) {
+                if (str.equals("x")) {
+                    xNum += 1;
+                } else {
+                    xNum += Integer.parseInt(str.replace("x", ""));
+                }
+            } else {
+                num += Integer.parseInt(str);
+            }
         }
         
-        return (xNum != 0 ? xNum > 1 ? xNum + "x" : "x" : "") 
-        		+ (num != 0 ? (xNum != 0 ? " + " : "") 
-                + num : xNum == 0 ? "0" : "");
+        String answer = "";
+        
+        if (xNum > 0) {
+            if (xNum == 1) answer = "x";
+            else answer = xNum + "x";
+        }
+        
+        if (num > 0) {
+            if (!answer.equals("")) answer += " + ";
+            answer += num;
+        }
+        
+        return answer;
     }
 }
