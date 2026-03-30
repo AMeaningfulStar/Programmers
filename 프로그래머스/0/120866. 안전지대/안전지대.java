aaf85f0a3556aa4b9f01866630a04957adeabc99
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-30 19:18:23 +0900

    [level 0] Title: 안전지대, Time: 0.06 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,27 +1,51 @@
+/*
+• 알고리즘: 2차원 배열 탐색 (2D Grid Traversal)
+• 핵심 개념
+ • 2차원 배열 순회
+ • 8방향 탐색
+ • 범위 체크
+ • 위험 지역 표시 후 안전 칸 카운팅
+• 시간 복잡도: O(N²)
+• 공간 복잡도: O(1) 또는 O(N²)
+ • 현재 코드: O(1) 추가 공간
+ • 추천 코드: danger 배열 사용 시 O(N²)
+*/
+
 class Solution {
     public int solution(int[][] board) {
-        int answer = 0;
-        int[][] list = new int[board.length+2][board.length+2];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                if(board[i][j] == 1){
-                    for(int a=i;a<=i+2;a++){
-                        for(int b=j;b<=j+2;b++){
-                            if(list[a][b] != 1){
-                                list[a][b] = 2;
-                            }
+        int n = board.length;
+        int[][] dir = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1},           {0, 1},
+            {1, -1},  {1, 0},  {1, 1}
+        };
+        
+        boolean[][] danger = new boolean[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    danger[i][j] = true;
+                    
+                    for (int[] d : dir) {
+                        int ni = i + d[0];
+                        int nj = j + d[1];
+                        
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                            danger[ni][nj] = true;
                         }
                     }
                 }
             }
         }
-        for(int i = 1;i<list.length-1;i++){
-            for(int j = 1;j<list.length-1;j++){
-                if(list[i][j] == 0){
-                    answer++;
-                }
+        
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!danger[i][j]) answer++;
             }
         }
+        
         return answer;
     }
 }
