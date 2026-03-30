4d4ff2a8d07c5bb052ce6b9397f49378ad516a6d
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-30 19:35:10 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,17 +1,26 @@
+/*
+• 알고리즘: 수학 + 유클리드 호제법
+• 핵심 개념
+ • 분수 통분
+ • 분자 계산
+ • 최대공약수(GCD)
+ • 기약분수 변환
+• 시간 복잡도: O(log N) (gcd)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int numer = numer1 * denom2 + numer2 * denom1;
         int denom = denom1 * denom2;
-        int max_num = 1;
         
-        for(int num = 1; num <= numer && num <= denom; num++) {
-            if (numer % num == 0 && denom % num == 0) {
-                max_num = num;
-            }
-        }
+        int gcd = gcd(numer, denom);
         
-        int[] answer = new int[]{ numer / max_num, denom / max_num };
-        
-        return answer;
+        return new int[]{numer / gcd, denom / gcd};
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
