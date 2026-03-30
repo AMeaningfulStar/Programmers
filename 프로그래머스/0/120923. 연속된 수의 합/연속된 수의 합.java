4fa818347c5c11656df7643dd3b6ec292a88d784
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-30 16:42:14 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,12 +1,21 @@
+/*
+• 알고리즘: 수학 계산 (Mathematical Calculation)
+• 핵심 개념
+ • 연속된 수열의 합 공식 활용
+ • 시작값 계산
+ • 등차수열 생성
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
+
 class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
         
-        int start_num = (total/num) - ((num - 1)/2);
+        int start = total / num - (num - 1) / 2;
         
-        for(int i = 0; i < num; i++) {
-            answer[i] = start_num;
-            start_num++;
+        for (int i = 0; i < num; i++) {
+            answer[i] = start + i;
         }
         
         return answer;
