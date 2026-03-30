859698b64da4c5dc33249b156e2a933a8a4d1f0d
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-03-30 19:57:55 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,11 +1,40 @@
+/*
+• 알고리즘: 기울기 비교 (Slope Comparison)
+• 핵심 개념
+ • 점 4개로 만들 수 있는 선분 쌍 3가지 확인
+ • 평행 조건 = 기울기 동일
+ • 나눗셈 대신 곱셈으로 비교
+• 시간 복잡도: O(1)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int[][] dots) {
-        boolean case1 = ((double) (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]) == ((double) (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]))) ? true: false;
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
         
-        boolean case2 = ((double) (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]) == (double) (dots[1][1] - dots[3][1]) / (dots[1][0] - dots[3][0])) ? true : false;
-        
-        boolean case3 = ((double) (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]) == (double) (dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0])) ? true : false;
-        
-        return case1 || case2 || case3 ? 1 : 0;
+        return 0;
+    }
+    
+    private boolean isParallel(int[] a, int[] b, int[] c, int[] d) {
+        return (b[1] - a[1]) * (d[0] - c[0]) == (d[1] - c[1]) * (b[0] - a[0]);
     }
 }
+
+/*
+다른 풀이
+class Solution {
+    public int solution(int[][] dots) {
+        if (getSlope(dots[0], dots[1]) == getSlope(dots[2], dots[3])) return 1;
+        if (getSlope(dots[0], dots[2]) == getSlope(dots[1], dots[3])) return 1;
+        if (getSlope(dots[0], dots[3]) == getSlope(dots[1], dots[2])) return 1;
+        
+        return 0;
+    }
+    
+    private double getSlope(int[] a, int[] b) {
+        return (double)(b[1] - a[1]) / (b[0] - a[0]);
+    }
+}
+*/
