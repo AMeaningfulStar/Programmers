0472a04cb8427d6dd7d7b0960d5eb4708dd184c4
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-02 23:26:59 +0900

    [level 1] Title: 콜라 문제, Time: 0.10 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,13 +1,42 @@
+/*
+• 알고리즘: 시뮬레이션 (Simulation)
+• 핵심 개념
+ • 몫 계산 (n / a)
+ • 나머지 계산 (n % a)
+ • 상태 갱신
+ • 반복 종료 조건
+• 시간 복잡도: O(log N) 수준의 반복
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-
-		while (n >= a) {
-			int newCola = (n / a) * b;
-			answer += newCola;
-			n = (n % a) + newCola; // 남은 병 + 새로 받은 콜라 병
-		}
-
-		return answer;
+        
+        while(n >= a) {
+            answer += (n / a) * b;
+            
+            n = ((n / a) * b) + (n % a);
+        } 
+        
+        return answer;
     }
 }
+
+/*
+다른 풀이
+1.
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while (n >= a) {
+            int exchanged = (n / a) * b;
+            answer += exchanged;
+            n = exchanged + (n % a);
+        }
+        
+        return answer;
+    }
+}
+*/
