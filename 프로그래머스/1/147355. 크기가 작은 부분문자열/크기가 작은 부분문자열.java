210ef52be708fd30ce648dd4a348081acb313465
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-02 21:30:02 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.25 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,15 +1,26 @@
+/*
+• 알고리즘: 문자열 슬라이딩 윈도우
+• 핵심 개념
+ • substring으로 부분 문자열 생성
+ • 문자열 비교 (compareTo)
+ • 길이 동일 → 사전순 비교 가능
+• 시간 복잡도: O(N × M)
+• 공간 복잡도: O(M)
+*/
+
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-
-		for (int idx = 0; idx <= t.length() - p.length(); idx++) {
-			String num = t.substring(idx, idx + p.length());
-
-			if (Long.parseLong(num) <= Long.parseLong(p)) {
-				answer++;
-			}
-		}
-
-		return answer;
-	}
+        int len = p.length();
+        
+        for (int i = 0; i <= t.length() - len; i++) {
+            String sub = t.substring(i, i + len);
+            
+            if (sub.compareTo(p) <= 0) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
 }
