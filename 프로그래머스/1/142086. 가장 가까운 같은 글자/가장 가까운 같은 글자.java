cb54de37fedb51455b9938cad6fb7d44dea4ba50
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-02 20:31:56 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 14.60 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,18 +1,56 @@
+/*
+• 알고리즘: 해시맵 기반 최근 위치 추적
+• 핵심 개념
+ • 문자별 마지막 등장 위치 저장
+ • 현재 인덱스와 이전 인덱스의 차 계산
+ • 처리 후 최신 위치로 갱신
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(K)
+ • K는 서로 다른 문자 개수
+*/
+
+import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
         int[] answer = new int[s.length()];
-		String[] strArr = s.split(""); // 한 번만 split 수행
-
-		for (int turn = 0; turn < s.length(); turn++) {
-			answer[turn] = -1;
-			for (int idx = turn - 1; idx >= 0; idx--) {
-				if (strArr[turn].equals(strArr[idx])) {
-					answer[turn] = turn - idx;
-					break;
-				}
-			}
-		}
-
-		return answer;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), i);
+                answer[i] = -1;
+            } else {
+                answer[i] = i - map.get(s.charAt(i));
+                map.put(s.charAt(i), i);
+            }
+        }
+        
+        return answer;
     }
 }
+
+/*
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int[] answer = new int[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (map.containsKey(c)) {
+                answer[i] = i - map.get(c);
+            } else {
+                answer[i] = -1;
+            }
+            
+            map.put(c, i);
+        }
+        
+        return answer;
+    }
+}
+*/
