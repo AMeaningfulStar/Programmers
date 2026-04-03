8c16fd44bdefcb07859d4e03faaf18dd2a5c1942
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-03 19:30:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,25 +1,28 @@
+/*
+• 알고리즘: 순차 탐색 / 투 포인터
+• 핵심 개념
+ • 각 배열의 현재 위치만 관리
+ • goal을 앞에서부터 순서대로 검사
+ • 범위 체크 후 비교
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        boolean answer = true;
-		
-		int cards1_idx = 0;
-		int cards2_idx = 0;
-		
-		for(String str: goal) {
-			if(!answer) break;
-			
-			if(cards1_idx < cards1.length && cards1[cards1_idx].equals(str)) {
-				cards1_idx++;
-				continue;
-			} else if(cards2_idx < cards2.length && cards2[cards2_idx].equals(str)) {
-				cards2_idx++;
-				continue;
-			} else {
-				answer = false;
-			}
-		}
-		
-
-		return answer ? "Yes" : "No";
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for (String word : goal) {
+            if (idx1 < cards1.length && cards1[idx1].equals(word)) {
+                idx1++;
+            } else if (idx2 < cards2.length && cards2[idx2].equals(word)) {
+                idx2++;
+            } else {
+                return "No";
+            }
+        }
+        
+        return "Yes";
     }
 }
