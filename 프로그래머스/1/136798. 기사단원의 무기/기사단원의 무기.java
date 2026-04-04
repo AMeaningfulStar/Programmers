bc1b87aca98bb0ca1d1e125538afba6bb46d0b75
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-04 21:42:16 +0900

    [level 1] Title: 기사단원의 무기, Time: 124.32 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,19 +1,32 @@
+/*
+• 알고리즘 유형: 약수 탐색 / 수학
+• 핵심 개념 목록
+ • 1부터 number까지 각 숫자의 약수 개수 구하기
+ • 약수는 쌍으로 존재하므로 sqrt(i)까지만 탐색
+ • 제곱수는 1개만 더하기
+ • limit 초과 시 power로 대체
+• 시간 복잡도: O(number × √number)
+• 공간 복잡도: O(1)
+*/
+
+import java.util.*;
+
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-
-		for (int i = 1; i <= number; i++) {
-			int count = 0;
-
-			for (int j = 1; j * j <= i; j++) {
-				if (i % j == 0) {
-					count += (j * j == i) ? 1 : 2; // 제곱수는 하나만 추가, 아니면 두 개
-				}
-			}
-
-			answer += count > limit ? power : count;
-		}
-
-		return answer;
+        
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+            
+            for (int num = 1; num * num <= i; num++) {
+                if (i % num == 0) {
+                    count += (num * num == i) ? 1 : 2;
+                }
+            }
+            
+            answer += (count > limit) ? power : count;
+        }
+        
+        return answer;
     }
 }
