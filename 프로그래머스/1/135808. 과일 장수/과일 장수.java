0c9e5a88d4ad6463be11cbefebf4ec140701311e
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-06 19:01:42 +0900

    [level 1] Title: 과일 장수, Time: 77.97 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,24 @@
+/*
+• 알고리즘 유형: 그리디 + 정렬
+• 핵심 개념
+ • 큰 값부터 묶기
+ • 그룹의 최소값이 중요
+• 시간 복잡도: O(N log N)
+• 공간 복잡도: O(1)
+*/
+
 import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-
-		Arrays.sort(score);
-
-		for (int idx = score.length - m; idx > -1; idx -= m) {
-			answer += score[idx] * m;
-		}
-
-		return answer;
+        
+        Arrays.sort(score);
+        
+        for(int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
+        } 
+        
+        return answer;
     }
 }
