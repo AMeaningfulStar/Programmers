d5ee7ec3753b0390e69aeaba6f42947a4c7bfef0
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-06 18:55:58 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.05 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,20 +1,57 @@
+/*
+• 알고리즘 유형: 구현 / 시뮬레이션 / 그리디
+• 핵심 개념
+ • 짧은 변 / 긴 변 기준 정렬
+ • 큰 변을 절반으로 접기
+ • 지갑에 들어갈 때까지 반복
+• 시간 복잡도: O(log bill) 수준
+ • 접을 때마다 큰 변이 절반으로 감소
+• 공간 복잡도: O(1)
+*/
+
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int walletMin = Math.min(wallet[0], wallet[1]);
+        int walletMax = Math.max(wallet[0], wallet[1]);
+        
+        int billMin = Math.min(bill[0], bill[1]);
+        int billMax = Math.max(bill[0], bill[1]);
+        
+        int answer = 0;
+        
+        while (billMin > walletMin || billMax > walletMax) {
+            billMax /= 2;
+            answer++;
+            
+            int newMin = Math.min(billMin, billMax);
+            int newMax = Math.max(billMin, billMax);
+            billMin = newMin;
+            billMax = newMax;
+        }
+        
+        return answer;
+    }
+}
+
+/*
+다른 풀이
+1.
 import java.util.*;
 
 class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
-		
-		// 정렬을 통해 작은 값은 0번 인덱스, 큰 값은 1번 인덱스로 위치
-		Arrays.sort(wallet);
-		Arrays.sort(bill);
-		
-		while(wallet[0] < bill[0] || wallet[1] < bill[1]) {
-			bill[1] = bill[1] / 2;
-			Arrays.sort(bill);
-			answer++;
-		}
-		
-		
-		return answer;
+        
+        Arrays.sort(wallet);
+        Arrays.sort(bill);
+        
+        while (bill[0] > wallet[0] || bill[1] > wallet[1]) {
+            bill[1] /= 2;
+            Arrays.sort(bill);
+            answer++;
+        }
+        
+        return answer;
     }
 }
+*/
