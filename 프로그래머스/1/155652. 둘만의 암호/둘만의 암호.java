874c567c6f0270b338caf12f1443dd9932b79657
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-07 15:23:24 +0900

    [level 1] Title: 둘만의 암호, Time: 0.74 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,25 +1,37 @@
+/*
+• 알고리즘 유형: 문자열 / 구현 / 시뮬레이션
+• 핵심 개념
+ • 알파벳 한 칸씩 이동
+ • z 다음은 a
+ • skip 문자는 건너뛰기
+ • 유효한 문자만 count 증가
+• 시간 복잡도: O(|s| × index) 정도
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-
-		for (char s_chr : s.toCharArray()) {
-			int count = 0;
-			char current = s_chr;
-
-			while (count < index) {
-				current++;
-				if (current > 'z') {
-					current = 'a'; // z를 넘어가면 다시 a부터 시작
-				}
-
-				if (!skip.contains(String.valueOf(current))) {
-					count++;
-				}
-			}
-
-			answer += current;
-		}
-
-		return answer;
+        StringBuilder answer = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            int count = 0;
+            char current = c;
+            
+            while (count < index) {
+                current++;
+                
+                if (current > 'z') {
+                    current = 'a';
+                }
+                
+                if (!skip.contains(String.valueOf(current))) {
+                    count++;
+                }
+            }
+            
+            answer.append(current);
+        }
+        
+        return answer.toString();
     }
 }
