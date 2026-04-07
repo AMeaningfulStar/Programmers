268b9e404ea1a6e477a3f4188302561aeb935946
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-07 15:06:16 +0900

    [level 1] Title: 문자열 나누기, Time: 0.84 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,33 +1,41 @@
+/*
+• 알고리즘 유형: 문자열 / 구현 / 그리디
+• 핵심 개념
+ • 기준 문자 설정
+ • 같은 문자 수 / 다른 문자 수 카운트
+ • 같아지는 순간 분리
+ • 마지막 남은 구간 처리
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(1)
+*/
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
-		int same = 0;
-		int diff = 0;
-		char target = s.charAt(0); // 첫 글자를 기준으로
-
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == target) {
-				same++;
-			} else {
-				diff++;
-			}
-
-			// 같은 수와 다른 수가 같아지면 끊고 다음 시작
-			if (same == diff) {
-				answer++;
-				if (i + 1 < s.length()) {
-					target = s.charAt(i + 1);
-				}
-				same = 0;
-				diff = 0;
-			}
-		}
-
-		// 문자열 끝까지 가도 남아있는 부분이 있다면 1 추가
-		if (same != 0 || diff != 0) {
-			answer++;
-		}
-
-		return answer;
+        
+        char target = 0;
+        int same = 0;
+        int different = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (same == 0 && different == 0) {
+                target = c;
+                same++;
+                continue;
+            }
+            
+            if (target == c) same++;
+            else different++;
+            
+            if (same == different) {
+                answer++;
+                same = 0;
+                different = 0;
+            }
+        }
+        
+        if (same != 0 || different != 0) answer++;
+        
+        return answer;
     }
 }
