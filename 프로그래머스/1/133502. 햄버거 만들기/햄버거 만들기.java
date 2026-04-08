ca06d3e3fc009623c8f3c21e4e6d1382c98df49e
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-08 17:13:23 +0900

    [level 1] Title: 햄버거 만들기, Time: 15.01 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,29 +1,56 @@
-import java.util.*;
+/*
+• 알고리즘 유형: 스택 / 구현
+• 핵심 개념
+ • 재료를 순서대로 쌓기
+ • 최근 4개가 1,2,3,1인지 확인
+ • 맞으면 제거
+• 시간 복잡도: O(N)
+• 공간 복잡도: O(N)
+*/
 
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
+        int[] stack = new int[ingredient.length];
+        int top = 0;
 
-        for (int i : ingredient) {
-            stack.push(i);
+        for (int num : ingredient) {
+            stack[top++] = num;
 
-            if (stack.size() >= 4) {
-                int size = stack.size();
-                if (stack.get(size - 4) == 1 &&
-                    stack.get(size - 3) == 2 &&
-                    stack.get(size - 2) == 3 &&
-                    stack.get(size - 1) == 1) {
-
-                    // 햄버거 완성 -> 4개 pop
-                    for (int j = 0; j < 4; j++) {
-                        stack.pop();
-                    }
-                    answer++;
-                }
+            if (top >= 4 &&
+                stack[top - 4] == 1 &&
+                stack[top - 3] == 2 &&
+                stack[top - 2] == 3 &&
+                stack[top - 1] == 1) {
+                
+                top -= 4;   // 햄버거 제거
+                answer++;
             }
         }
 
         return answer;
     }
 }
+
+/*
+다른 풀이
+1.
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : ingredient) {
+            sb.append(num);
+
+            if (sb.length() >= 4 &&
+                sb.substring(sb.length() - 4).equals("1231")) {
+                sb.delete(sb.length() - 4, sb.length());
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
+*/
