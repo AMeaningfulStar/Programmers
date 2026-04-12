107105b13a79775c9ff2d6951e4190a2b0016a2c
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-12 14:06:55 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,21 +1,38 @@
+/*
+• 알고리즘 유형: 구현 / 시뮬레이션 / 2차원 배열
+• 핵심 개념
+ • 지그재그 배치
+ • num의 열 찾기
+ • 같은 열의 위쪽 상자 개수 세기
+• 시간 복잡도: O((n / w) × w) ≈ O(n)
+• 공간 복잡도: O(n)
+*/
+
+import java.util.*;
+
 class Solution {
     public int solution(int n, int w, int num) {
+        int[][] box_list = new int[(n + w - 1) / w][w];
+        int idx = 0;
+        
+        for(int i = 0; i < box_list.length; i++) {
+            for(int j = 0; j < w; j++) {
+                int result = i % 2 == 0 ? (i * w) + j + 1 : ((i + 1) * w) - j;
+                if (result <= n) {
+                    box_list[i][j] = result;
+                    if (result == num) idx = j;
+                } else {
+                    box_list[i][j] = 0;
+                }
+            }
+        }
+        
         int answer = 0;
-		
-		int n_row = (n - 1) / w;
-		int n_index = n_row % 2 == 0 ? (n - 1) % w : w - 1 - ((n - 1) % w);
-
-		int num_row = (num - 1) / w;
-		int num_index = num_row % 2 == 0 ? (num - 1) % w : w - 1 - ((num - 1) % w);
-		
-		answer = n_row - num_row;
-		
-		if(n_row % 2 == 0) {
-			answer += (n_index >= num_index) ? 1 : 0;
-		} else {
-			answer += (n_index <= num_index) ? 1 : 0;
-		}
-		
+        
+        for(int i = 0; i < box_list.length; i++) {
+            if(box_list[i][idx] >= num) answer++;
+        }
+        
         return answer;
     }
 }
