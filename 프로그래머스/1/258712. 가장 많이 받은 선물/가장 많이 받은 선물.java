4daf34055dfd39e542dd2fa129e54eb3442dc22f
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-04-12 14:46:09 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 30.95 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,44 +1,60 @@
+/*
+• 알고리즘 유형: 구현 / 해시 / 2차원 배열
+• 핵심 개념
+ • 친구 이름 → 인덱스 변환
+ • 서로 준 선물 개수 기록
+ • 선물지수 계산
+ • 모든 친구 쌍 비교
+ • 동률일 때 선물지수 비교, 그것도 같으면 아무도 못 받음
+• 시간 복잡도: O(F² + G)
+ • F: 친구 수
+ • G: 선물 기록 수
+• 공간 복잡도: O(F²)
+*/
+
 import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-
-		int friendsLength = friends.length;
-		Map<String, Integer> dic = new HashMap<String, Integer>();
-		int[] giftDegree = new int[friendsLength];
-		int[][] giftGraph = new int[friendsLength][friendsLength];
-
-		for (int idx = 0; idx < friendsLength; idx++) {
-			dic.put(friends[idx], idx);
-		}
-
-		for (String gift : gifts) {
-			String[] giftName = gift.split(" ");
-			giftDegree[dic.get(giftName[0])]++;
-			giftDegree[dic.get(giftName[1])]--;
-			giftGraph[dic.get(giftName[0])][dic.get(giftName[1])]++;
-		}
-
-		for (int x_idx = 0; x_idx < friendsLength; x_idx++) {
-			int num = 0;
-
-			for (int y_idx = 0; y_idx < friendsLength; y_idx++) {
-				if (x_idx == y_idx) {
-					continue;
-				}
-
-				if (giftGraph[x_idx][y_idx] > giftGraph[y_idx][x_idx]
-						|| (giftGraph[x_idx][y_idx] == giftGraph[y_idx][x_idx] && giftDegree[x_idx] > giftDegree[y_idx])) {
-					num++;
-				}
-			}
-			
-			if(answer < num) {
-				answer = num;
-			}
-		}
-
-		return answer;
+        Map<String, Integer> friend = new HashMap<>();
+        int friendsLen = friends.length;
+        
+        for (int idx = 0; idx < friends.length; idx++) {
+            friend.put(friends[idx], idx);
+        }
+        
+        int[][] giftMap = new int[friendsLen][friendsLen];
+        int[] score = new int[friendsLen];
+        
+        for (String gift : gifts) {
+            String[] names = gift.split(" ");
+            
+            int from = friend.get(names[0]);
+            int to = friend.get(names[1]);
+            
+            giftMap[from][to]++;
+            score[from]++;
+            score[to]--;
+        }
+        
+        int[] count = new int[friendsLen];
+        
+        for (int i = 0; i < friendsLen - 1; i++) {
+            for (int j = i + 1; j < friendsLen; j++) {
+                if (giftMap[i][j] > giftMap[j][i]) {
+                    count[i]++;
+                } else if (giftMap[i][j] < giftMap[j][i]) {
+                    count[j]++;
+                } else {
+                    if (score[i] > score[j]) {
+                        count[i]++;
+                    } else if (score[i] < score[j]) {
+                        count[j]++;
+                    }
+                }
+            }
+        }
+        
+        return Arrays.stream(count).max().getAsInt();
     }
 }
