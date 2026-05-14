defbd4ac6971e418222a932d64eea58f8d9ac53d
A Meaningful star <79067888+AMeaningfulStar@users.noreply.github.com>
2026-05-15 00:35:51 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 93 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int num = N; num >= 0; num--) {
+			System.out.printf("%d ", num);
+		}
+	}
+}
